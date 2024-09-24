9170d3658f8dfe86f6d4567c1e6d675b8c0e373d
stephenroome <me@stephenroome.com>
2024-09-24 18:55:17 +0100

    Updated paths to /user/work/${USER}

--- pycontrails/models/boxmodel/sc.sh
+++ pycontrails/models/boxmodel/sc.sh
@@ -8,10 +8,21 @@
 #SBATCH --mem=64G
 #SBATCH --account=aero004481
 
+#SBATCH --ntasks-per-node=1
+#
+
+#Apparently this requires the entire memory on the node, which is what we want rather than --exclusive
+
+#Don't care if others use the node but use no memory on it! Tricky to manage probably.
+
+#SBATCH --mem=0
+
 #aero004301 
 #aero004481 
    #default 
 #isys015562
 
+NCLIBS=`nc-config --libdir`
+NFLIBS=`nf-config --prefix`/lib
 
-PYCONTRAILSDIR=/user/work/kt16229/pycontrails_kt/pycontrails ./boxm_script.py
+LD_LIBRARY_PATH=${NCLIBS}:${NFLIBS} PYCONTRAILSDIR=/user/work/${USER}/pycontrails_kt/pycontrails ./boxm_script.py
