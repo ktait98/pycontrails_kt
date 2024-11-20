eac59442150fb90ae7aae7078153f9dece894d5f
ktait98 <kt16229@bristol.ac.uk>
2024-11-20 21:11:20 +0000

    added hres to name

--- pycontrails/models/gpat/run_gpat_mc_v.py
+++ pycontrails/models/gpat/run_gpat_mc_v.py
@@ -66,11 +66,12 @@ print("PlumeParams:", asdict(plume_params))
 
 update_sim_params_from_args(sim_params, updated_args)
 sim_params.job_id = (f"mc_v_{fl_params.n_ac}_"
-                f"{fl_params.sep_dist[0]}_"
-                f"{fl_params.sep_dist[1]}_"
+                f"{int(fl_params.sep_dist[0])}_"
+                f"{int(fl_params.sep_dist[1])}_"
                 f"{plume_params.n_slices}_"
                 f"{plume_params.max_age.components.hours}_"
-                f"{plume_params.dt_integration.components.hours}")
+                f"{plume_params.dt_integration.components.hours}_"
+                f"{plume_params.hres_pl}")
 
 print("SimParams:", asdict(sim_params)) 
 
